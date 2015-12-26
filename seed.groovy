014d40fcbf7ba3bc9b0cc848d3ac387dcda2d957
Seth Baxter <sethbaxter@prominentedge.com>
2015-12-26 12:52:38 -0500

    Hack a pipeline

--- seed.groovy
+++ seed.groovy
@@ -1,36 +1,29 @@
-def projects = [
-  [name: 'refapp-devops', jobs: ['setup', 'artifact', 'build-test', 'deliver']]
-]
+class PJob {
+  def project
+  def step
+  def job
 
-for (p in projects) {
-  for (job in p.jobs) {
-
-    freeStyleJob ("${p.name}-${job}") {
+  def base() {
+    this.job.with {
       properties {
-        githubProjectUrl "https://github.com/venicegeo/${p.name}"
+        githubProjectUrl "https://github.com/venicegeo/${this.project}"
       }
 
       scm {
         git {
           remote {
-            github "venicegeo/${p.name}"
+            github "venicegeo/${this.project}"
             credentials 'fa3aab48-4edc-446d-b1e2-1d89d86f4458'
           }
         }
       }
 
-      triggers {
-        if (job != 'deliver') {
-          githubPush()
-        }
+      steps {
+        shell("git clean -xffd")
+        shell("[ -x ./build/${this.step}.sh ] && ./build/${this.step}.sh || echo noop")
       }
 
       logRotator { numToKeep 30 }
-
-      steps {
-        shell("git clean -xffd")
-        shell("./build/${job}.sh")
-      }
 
       publishers {
         slackNotifications {
@@ -39,35 +32,98 @@ for (p in projects) {
           notifyUnstable()
           notifyBackToNormal()
         }
+        downstream("${this.project}-teardown", "FAILURE")
+        downstream("${this.project}-teardown", "UNSTABLE")
       }
 
       wrappers {
         colorizeOutput()
       }
+    }
 
-      if (job == 'deliver') {
-        configure { project ->
-          project / publishers << 'com.hpe.cloudfoundryjenkins.CloudFoundryPushPublisher' {
-            target 'http://api.cf.piazzageo.io'
-            organization 'piazza'
-            cloudSpace 'dev'
-            credentialsId 'ff5565ae-2494-45c0-ac9a-d01003a34096'
-            selfSigned true
-            resetIfExists false
-            pluginTimeout 120
-            servicesToCreate ''
-            appURIs ''
-            manifestChoice {
-              value 'manifestFile'
-              manifestFile 'manifest.yml'
-              memory 0
-              instances 0
-              noRoute false
-            }
+    return this
+  }
+
+  def trigger() {
+    this.job.with {
+      triggers {
+        githubPush()
+      }
+    }
+
+    return this
+  }
+
+  def deliver() {
+    this.job.with {
+      configure { project ->
+        project / publishers << 'com.hpe.cloudfoundryjenkins.CloudFoundryPushPublisher' {
+          target 'http://api.cf.piazzageo.io'
+          organization 'piazza'
+          cloudSpace 'dev'
+          credentialsId 'ff5565ae-2494-45c0-ac9a-d01003a34096'
+          selfSigned true
+          resetIfExists false
+          pluginTimeout 120
+          servicesToCreate ''
+          appURIs ''
+          manifestChoice {
+            value 'manifestFile'
+            manifestFile 'manifest.yml'
+            memory 0
+            instances 0
+            noRoute false
           }
         }
       }
     }
 
+    return this
+  }
+}
+
+def projects = [
+  [
+    name: 'refapp-devops',
+    pipeline: ['setup','build','build-test','artifact','artifact-test','deliver','teardown']
+  ]
+]
+
+
+for (p in projects) {
+  def jobs = [:]
+  p.pipeline.eachWithIndex { s, i ->
+    jobs[s] = new PJob([
+      project: p.name,
+      step: s,
+      job: job("${p.name}-${s}")
+    ])
+
+    jobs[s].base()
+
+    if (i == 0) {
+      jobs[s].trigger()
+    }
+
+    if (s == 'deliver') {
+      jobs[s].deliver()
+    } else {
+      jobs[s].job.with {
+        publishers {
+          downstream("${p.name}-${p.pipeline[i+1]}", "SUCCESS")
+        }
+      }
+    }
+  }
+
+  deliveryPipelineView(p.name) {
+    allowPipelineStart(true)
+    allowRebuild(true)
+    pipelineInstances(5)
+    columns(5)
+    updateInterval(60)
+    pipelines {
+      component(p.name, "${p.name}-${p.pipeline[0]}")
+    }
   }
 }
