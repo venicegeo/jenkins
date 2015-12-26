9315cfd75feff799b5f56aa0ecfc4562374b422e
Seth Baxter <sethbaxter@prominentedge.com>
2015-12-26 09:21:11 -0500

    Add cloudfoundry config

--- seed.groovy
+++ seed.groovy
@@ -1,19 +1,19 @@
 def projects = [
-  [name: 'refapp-devops', jobs: ['setup', 'artifact', 'build-test']]
+  [name: 'refapp-devops', jobs: ['setup', 'artifact', 'build-test', 'deliver']]
 ]
 
-for (project in projects) {
-  for (job in project.jobs) {
+for (p in projects) {
+  for (job in p.jobs) {
 
-    freeStyleJob ("${project.name}-${job}") {
+    freeStyleJob ("${p.name}-${job}") {
       properties {
-        githubProjectUrl "https://github.com/venicegeo/${project.name}"
+        githubProjectUrl "https://github.com/venicegeo/${p.name}"
       }
 
       scm {
         git {
           remote {
-            github "venicegeo/${project.name}"
+            github "venicegeo/${p.name}"
             credentials 'fa3aab48-4edc-446d-b1e2-1d89d86f4458'
           }
         }
@@ -44,6 +44,28 @@ for (project in projects) {
 
       wrappers {
         colorizeOutput()
+      }
+
+      if (job == 'deliver') {
+        configure { project ->
+          project / publisher << 'com.hpe.cloudfoundryjenkins.CloudFoundryPushPublisher' {
+            target 'http://api.cf.piazzageo.io</target>'
+            organization 'piazza'
+            space 'dev'
+            credentialsId 'ff5565ae-2494-45c0-ac9a-d01003a34096'
+            cloudSpace 'dev'
+            selfSigned true
+            resetIfExists false
+            pluginTimeout 120
+            manifestChoice {
+              value 'manifestFile'
+              manifestFile 'manifest.yml'
+              memory 0
+              instances 0
+              noRoute false
+            }
+          }
+        }
       }
     }
 
