48d3db9ef5b9b726acb7437e82be088fc0ec2cf9
Seth Baxter <sethbaxter@prominentedge.com>
2015-12-30 13:08:31 -0500

    Cleanup

---
+++ pipeline.groovy
@@ -0,0 +1,84 @@
+package pipeline
+
+class PipelineJob {
+  def project
+  def step
+  def job
+
+  def base() {
+    this.job.with {
+      properties {
+        githubProjectUrl "https://github.com/venicegeo/${this.project}"
+      }
+
+      scm {
+        git {
+          remote {
+            github "venicegeo/${this.project}"
+            credentials 'fa3aab48-4edc-446d-b1e2-1d89d86f4458'
+          }
+        }
+      }
+
+      steps {
+        shell("git clean -xffd")
+        shell("[ -x ./scripts/${this.step}.sh ] && ./scripts/${this.step}.sh || echo noop")
+      }
+
+      logRotator { numToKeep 30 }
+
+      publishers {
+        slackNotifications {
+          notifyFailure()
+          notifyNotBuilt()
+          notifyUnstable()
+          notifyBackToNormal()
+        }
+      }
+
+      wrappers {
+        colorizeOutput()
+      }
+    }
+
+    return this
+  }
+
+  def trigger() {
+    this.job.with {
+      triggers {
+        githubPush()
+      }
+    }
+
+    return this
+  }
+
+  def deliver() {
+    this.job.with {
+      configure { project ->
+        project / publishers << 'com.hpe.cloudfoundryjenkins.CloudFoundryPushPublisher' {
+          target 'http://api.cf.piazzageo.io'
+          organization 'piazza'
+          cloudSpace 'dev'
+          credentialsId 'ff5565ae-2494-45c0-ac9a-d01003a34096'
+          selfSigned true
+          resetIfExists false
+          pluginTimeout 120
+          servicesToCreate ''
+          appURIs ''
+          manifestChoice {
+            value 'manifestFile'
+            manifestFile 'manifest.yml'
+            memory 0
+            instances 0
+            noRoute false
+          }
+        }
+      }
+    }
+
+    return this
+  }
+}
+
--- seed.groovy
+++ seed.groovy
@@ -1,99 +1,16 @@
-class PJob {
-  def project
-  def step
-  def job
-
-  def base() {
-    this.job.with {
-      properties {
-        githubProjectUrl "https://github.com/venicegeo/${this.project}"
-      }
-
-      scm {
-        git {
-          remote {
-            github "venicegeo/${this.project}"
-            credentials 'fa3aab48-4edc-446d-b1e2-1d89d86f4458'
-          }
-        }
-      }
-
-      steps {
-        shell("git clean -xffd")
-        shell("[ -x ./build/${this.step}.sh ] && ./build/${this.step}.sh || echo noop")
-      }
-
-      logRotator { numToKeep 30 }
-
-      publishers {
-        slackNotifications {
-          notifyFailure()
-          notifyNotBuilt()
-          notifyUnstable()
-          notifyBackToNormal()
-        }
-        downstream("${this.project}-teardown", "FAILURE")
-        downstream("${this.project}-teardown", "UNSTABLE")
-      }
-
-      wrappers {
-        colorizeOutput()
-      }
-    }
-
-    return this
-  }
-
-  def trigger() {
-    this.job.with {
-      triggers {
-        githubPush()
-      }
-    }
-
-    return this
-  }
-
-  def deliver() {
-    this.job.with {
-      configure { project ->
-        project / publishers << 'com.hpe.cloudfoundryjenkins.CloudFoundryPushPublisher' {
-          target 'http://api.cf.piazzageo.io'
-          organization 'piazza'
-          cloudSpace 'dev'
-          credentialsId 'ff5565ae-2494-45c0-ac9a-d01003a34096'
-          selfSigned true
-          resetIfExists false
-          pluginTimeout 120
-          servicesToCreate ''
-          appURIs ''
-          manifestChoice {
-            value 'manifestFile'
-            manifestFile 'manifest.yml'
-            memory 0
-            instances 0
-            noRoute false
-          }
-        }
-      }
-    }
-
-    return this
-  }
-}
+import pipeline.PipelineJob
 
 def projects = [
   [
     name: 'refapp-devops',
-    pipeline: ['setup','build','build-test','artifact','artifact-test','deliver','teardown']
+    pipeline: ['setup','test','artifact','placeholder','cf-deliver','health-check']
   ]
 ]
-
 
 for (p in projects) {
   def jobs = [:]
   p.pipeline.eachWithIndex { s, i ->
-    jobs[s] = new PJob([
+    jobs[s] = new PipelineJob([
       project: p.name,
       step: s,
       job: job("${p.name}-${s}")
@@ -105,7 +22,7 @@ for (p in projects) {
       jobs[s].trigger()
     }
 
-    if (s == 'deliver') {
+    if (s == 'cf-deliver') {
       jobs[s].deliver()
     } else {
       jobs[s].job.with {
