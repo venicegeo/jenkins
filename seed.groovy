456927ce821ec313e0e34af9cd1f7fb19a50ac36
Kevin Naquin <kevinnaquin@Kevins-MacBook-Pro.local>
2016-01-21 09:57:18 -0500

    Added cf-deliver and health-cehck to the pzsvc-coorcinate-conversion pipeline

--- seed.groovy
+++ seed.groovy
@@ -27,7 +27,7 @@ def projects = [
     pipeline: ['build-test-archive','cf-deliver','health-check']
   ],[
     name: 'pzsvc-coordinate-conversion',
-    pipeline: ['setup', 'build']
+    pipeline: ['build', 'cf-deliver', 'health-check']
   ],[
     name: 'pz-gateway',
     pipeline: ['build']
