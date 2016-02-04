4fd00b198a433f14f5de30b3689d1b361c38dce8
Seth Baxter <sethbaxter@prominentedge.com>
2016-02-04 13:06:47 -0500

    And the awk variable

--- PipelineJob.groovy
+++ PipelineJob.groovy
@@ -65,7 +65,7 @@ class PipelineJob {
     this.job.with {
       steps {
         shell("""
-          current=`cf routes | grep ${this.project} | awk '{print $4}'`
+          current=`cf routes | grep ${this.project} | awk '{print \$4}'`
           cf map-route ${this.project}-\$GIT_REVISION ${this.cfdomain} -n ${this.project}
           cf delete -f \$current
         """)
