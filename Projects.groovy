bd35082a2d22c3badf197674906f7cd2ca4ae663
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-02-26 11:59:42 -0500

    Put everything in alphabetical order.

--- Projects.groovy
+++ Projects.groovy
@@ -15,57 +15,18 @@
 class Projects {
   static list = [
     [
-      name: 'refapp-devops',
-      pipeline: ['setup','test','artifact','placeholder','cf-deliver','health-check','cf-deploy'],
-      branch: 'master'
-    ],[
-      name: 'pz-workflow',
-      pipeline: ['build-test-archive','cf-deliver','health-check'],
+      name: 'pz-access',
+      pipeline: ['build', 'cf-deliver', 'health-check'],
       branch: 'master'
     ],[
       name: 'pz-discover',
       pipeline: ['setup','test','artifact','cf-deliver','health-check']
     ],[
-      name: 'pz-logger',
-      pipeline: ['build-test-archive','cf-deliver','health-check'],
-      branch: 'master'
-    ],[
-      name: 'pz-uuidgen',
-      pipeline: ['build-test-archive','cf-deliver','health-check'],
-      branch: 'master'
-    ],[
-      name: 'pz-swagger',
-      pcf: true,
-      pipeline: ['cf-deliver', 'health-check', 'cf-deploy']
-    ],[
-      name: 'pz-servicecontroller',
-      pipeline: ['build', 'cf-deliver', 'health-check', 'black-box-tests']
-    ],[
-      name: 'pz-jobcommon',
-      pipeline: ['build'],
-      branch: 'master'
-    ],[
-      name: 'pzsvc-gdaldem',
-      pipeline: ['build-test-archive','cf-deliver','health-check']
-    ],[
-      name: 'pzsvc-lasinfo',
-      pipeline: ['build-test-archive','cf-deliver','health-check']
-    ],[
-      name: 'pzsvc-pdal',
-      pipeline: ['build-test-archive','cf-deliver','health-check', 'black-box-tests']
-    ],[
-      name: 'pzsvc-coordinate-conversion',
-      pipeline: ['build', 'cf-deliver', 'health-check']
-    ],[
-      name: 'pz-gateway',
-      pipeline: ['build', 'cf-deliver', 'health-check'],
-      branch: 'master'
-    ],[
       name: 'pz-dispatcher',
       pipeline: ['build', 'cf-deliver', 'health-check'],
       branch: 'master'
     ],[
-      name: 'pz-jobmanager',
+      name: 'pz-gateway',
       pipeline: ['build', 'cf-deliver', 'health-check'],
       branch: 'master'
     ],[
@@ -73,25 +34,63 @@ class Projects {
       pipeline: ['build', 'cf-deliver'],
       branch: 'master'
     ],[
-      name: 'pz-access',
+      name: 'pz-jobcommon',
+      pipeline: ['build'],
+      branch: 'master'
+    ],[
+      name: 'pz-jobmanager',
       pipeline: ['build', 'cf-deliver', 'health-check'],
       branch: 'master'
     ],[
-      name: 'pzsvc-us-geospatial-filter',
-      pipeline: ['build', 'cf-deliver', 'health-check']
-    ],[
-     name: 'pzsvc-us-phone-number-filter',
-     pipeline: ['build', 'cf-deliver', 'health-check']
-    ],[
-      name: 'pzclient-sak',
-      pipeline: ['cf-deliver'],
+      name: 'pz-logger',
+      pipeline: ['build-test-archive','cf-deliver','health-check'],
       branch: 'master'
     ],[
       name: 'pz-search-metadata-ingest',
       pipeline: ['build', 'cf-deliver', 'health-check'],
       branch: 'master'
-    ],
-    [
+    ],[
+      name: 'pz-servicecontroller',
+      pipeline: ['build', 'cf-deliver', 'health-check', 'black-box-tests']
+    ],[
+      name: 'pz-swagger',
+      pcf: true,
+      pipeline: ['cf-deliver', 'health-check', 'cf-deploy']
+    ],[
+      name: 'pz-uuidgen',
+      pipeline: ['build-test-archive','cf-deliver','health-check'],
+      branch: 'master'
+    ],[
+      name: 'pz-workflow',
+      pipeline: ['build-test-archive','cf-deliver','health-check'],
+      branch: 'master'
+    ],[
+      name: 'pzclient-sak',
+      pipeline: ['cf-deliver'],
+      branch: 'master'
+    ],[
+      name: 'pzsvc-coordinate-conversion',
+      pipeline: ['build', 'cf-deliver', 'health-check']
+    ],[
+      name: 'pzsvc-gdaldem',
+      pipeline: ['build-test-archive','cf-deliver','health-check']
+    ],[
+     name: 'pzsvc-lasinfo',
+      pipeline: ['build-test-archive','cf-deliver','health-check']
+    ],[
+      name: 'pzsvc-pdal',
+      pipeline: ['build-test-archive','cf-deliver','health-check', 'black-box-tests']
+    ],[
+      name: 'pzsvc-us-geospatial-filter',
+      pipeline: ['build', 'cf-deliver', 'health-check']
+    ],[
+      name: 'pzsvc-us-phone-number-filter',
+      pipeline: ['build', 'cf-deliver', 'health-check']
+    ],[
+      name: 'refapp-devops',
+      pipeline: ['setup','test','artifact','placeholder','cf-deliver','health-check','cf-deploy'],
+      branch: 'master'
+    ],[
       name: 'time-lapse-viewer',
       pipeline: ['build', 'cf-deliver', 'health-check']
     ]
