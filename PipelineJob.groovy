0a8b1da8ed7bdc2331ccc0b6650587a0d4f9e456
Seth Baxter <sethbaxter@prominentedge.com>
2016-03-11 05:54:30 -0500

    Return of slack notifications

--- PipelineJob.groovy
+++ PipelineJob.groovy
@@ -46,6 +46,26 @@ class PipelineJob {
       }
 
       logRotator { numToKeep 30 }
+
+      configure { project ->
+        project / properties << 'jenkins.plugins.slack.SlackNotifier_-SlackJobProperty' {
+          teamDomain "https://venicegeo.slack.com"
+          token credentialsId("53c859fe-e776-11e5-9730-9a79f06e9478")
+          room "jenkins"
+          startNotification false
+          notifySuccess false
+          notifyAborted true
+          notifyNotBuilt true
+          notifyUnstable true
+          notifyFailure true
+          notifyBackToNormal true
+          notifyRepeatedFailure true
+          includeTestSummary false
+          showCommitList false
+          includeCustomMessage true
+          customMessage "<$GIT_COMMIT>"
+        }
+      }
     }
 
     return this
