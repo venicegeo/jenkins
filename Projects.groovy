2315151d0c85ecefaf57a4507ae8408940e1ebdf
Sonny Saniev <sonny.saniev@rgi-corp.com>
2016-03-23 17:13:44 -0400

    Adding pzsvc-previewgenerator to builds

--- Projects.groovy
+++ Projects.groovy
@@ -95,6 +95,9 @@ class Projects {
       pipeline: ['test', 'archive', 'stage', 'blackbox'],
       branch: 'geoint'
     ],[
+      name: 'pzsvc-previewgenerator',
+	  pipeline: ['archive', 'stage']
+    ],[
       name: 'pzsvc-us-phone-number-filter',
       pipeline: ['archive', 'stage']
     ],[
