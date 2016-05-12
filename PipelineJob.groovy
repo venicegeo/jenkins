668a9e19949903a2e9f6ca8cd72b4279d4f4ac7c
Seth Baxter <sethbaxter@prominentedge.com>
2016-05-12 17:23:25 -0400

    Formatting?

--- PipelineJob.groovy
+++ PipelineJob.groovy
@@ -77,7 +77,7 @@ class PipelineJob {
       }
 
       parameters {
-        choiceParam('domain', this.domains,'PCF Domain/Space to target\ngeointservices.io = prod\nstage.geointservices.io = beta\nint.geointservices.io = CI')
+        choiceParam('domain', this.domains,'PCF Domain/Space to target<br>geointservices.io<br>production,stage.geointservices.io<br>beta,int.geointservices.io = CI')
         stringParam('revision', 'latest', 'commit sha, git branch or tag to build (default: latest revision)')
       }
 
