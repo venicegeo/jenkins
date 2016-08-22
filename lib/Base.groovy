afe6d8051ae7e8a122fecedfbf204b2f658caf87
Seth Baxter <sethbaxter@prominentedge.com>
2016-08-22 17:50:56 -0400

    Uh, for old jenkins

--- lib/Base.groovy
+++ lib/Base.groovy
@@ -79,9 +79,7 @@ class Base {
             github("${this.config.gh_org}/${this.config.gh_repo}", 'ssh')
             credentials "95eee62c-dc20-44d5-a141-14a11856421e"
           }
-          extensions {
-            localBranch("${this.config.gh_branch}")
-          }
+          localBranch("${this.config.gh_branch}")
           branch("${this.config.gh_branch}")
         }
       }
