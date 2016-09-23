4cb7c2534b749ebdcca8add37a4e6720c9414d69
sbaxter <seth@neuralux.com>
2016-09-23 06:41:14 -0400

    Remove beachfront int tests from ui pipeline

--- Repos.groovy
+++ Repos.groovy
@@ -23,7 +23,7 @@ class Repos {
       reponame: 'bf-ui',
       team: 'beachfront',
       manual: ['archive', 'cf_push', 'cf_bg_deploy', 'cf_promote_to_stage', 'cf_promote_to_prod'],
-      pipeline: ['archive', 'cf_push_int', 'cf_bg_deploy_int', 'beachfront_ua_tests_int', 'beachfront_integration_tests', 'cf_push_stage', 'cf_bg_deploy_stage']
+      pipeline: ['archive', 'cf_push_int', 'cf_bg_deploy_int', 'beachfront_ua_tests_int', 'cf_push_stage', 'cf_bg_deploy_stage']
     ],[
       reponame: 'bftest-ui',
       team: 'beachfront',
