98f7c228e8db0873a08508292780dd9d5277ed4d
sbaxter <seth@neuralux.com>
2016-09-29 09:15:58 -0400

    remove ion from pz-servicecontroller for now

--- Repos.groovy
+++ Repos.groovy
@@ -97,7 +97,7 @@ class Repos {
     ],[
       reponame: 'pz-servicecontroller',
       team: 'piazza',
-      pipeline: ['ionchannel_pom', 'archive', 'cf_push_int', 'cf_bg_deploy_int', 'int-release', 'run_integration_tests', 'cf_push_stage', 'cf_bg_deploy_stage', 'stage-release']
+      pipeline: ['archive', 'cf_push_int', 'cf_bg_deploy_int', 'int-release', 'run_integration_tests', 'cf_push_stage', 'cf_bg_deploy_stage', 'stage-release']
     ],[
       reponame: 'pz-servicemonitor',
       team: 'sandbox',
