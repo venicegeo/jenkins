25921e29a45336176e2ca929e60b94d3c780a8f3
sbaxter <seth@neuralux.com>
2016-10-06 06:58:16 -0400

    Sonar with pz-docs

--- Repos.groovy
+++ Repos.groovy
@@ -51,7 +51,7 @@ class Repos {
     ],[
       reponame: 'pz-docs',
       team: 'piazza',
-      pipeline: ['archive', 'cf_push_int', 'cf_bg_deploy_int', 'int-release', 'run_integration_tests', 'cf_push_stage', 'cf_bg_deploy_stage', 'stage-release']
+      pipeline: ['sonar', 'archive', 'cf_push_int', 'cf_bg_deploy_int', 'int-release', 'run_integration_tests', 'cf_push_stage', 'cf_bg_deploy_stage', 'stage-release']
     ],[
       reponame: 'pz-gateway',
       team: 'piazza',
