5d3055e8ae52fa04c88e588c553ec660ba25dc7e
sbaxter <seth@neuralux.com>
2016-10-06 07:08:49 -0400

    Add sonar to pz-logger

--- Repos.groovy
+++ Repos.groovy
@@ -77,7 +77,7 @@ class Repos {
     ],[
       reponame: 'pz-logger',
       team: 'piazza',
-      pipeline: ['test', 'archive', 'cf_push_int', 'cf_bg_deploy_int', 'int-release', 'run_integration_tests', 'cf_push_stage', 'cf_bg_deploy_stage', 'stage-release']
+      pipeline: ['sonar', 'test', 'archive', 'cf_push_int', 'cf_bg_deploy_int', 'int-release', 'run_integration_tests', 'cf_push_stage', 'cf_bg_deploy_stage', 'stage-release']
     ],[
       reponame: 'pz-sak',
       team: 'piazza',
