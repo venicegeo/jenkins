37073099a36c62aa9fa401fb85971950fb519c46
Patrick D <go0ty@users.noreply.github.com>
2016-10-17 13:58:49 -0400

    Add ION Channel to Service Controller

--- Repos.groovy
+++ Repos.groovy
@@ -104,7 +104,7 @@ class Repos {
     ],[
       reponame: 'pz-servicecontroller',
       team: 'piazza',
-      pipeline: ['sonar', 'archive', 'cf_push_int', 'cf_bg_deploy_int', 'int-release', 'run_integration_tests', 'cf_push_stage', 'cf_bg_deploy_stage', 'stage-release', 'fortify']
+      pipeline: ['sonar', 'ionchannel_pom', 'archive', 'cf_push_int', 'cf_bg_deploy_int', 'int-release', 'run_integration_tests', 'cf_push_stage', 'cf_bg_deploy_stage', 'stage-release', 'fortify']
     ],[
       reponame: 'pz-servicemonitor',
       team: 'sandbox',
