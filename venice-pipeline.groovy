be5abfa2ba7fe701d45d7fb3cdfa4209b3ccfb60
Ben Hosmer <ben.hosmer@gmail.com>
2016-10-19 20:10:19 -0400

    Initial pipeline seed

---
+++ venice-pipeline.groovy
@@ -0,0 +1,29 @@
+#!groovy
+// This is the initial seed job for venicegeo which creates all of ther
+// other pipeline jobs. Each repo must contain a JenkinsFile
+// that denotes the steps to take when building.
+
+InputStream  configFile = streamFileFromWorkspace('projects.yml')
+
+
+def gitprefix = 'https://github.com/venicegeo/pz-'
+def projects = ['ingest', 'idam', 'gateway']
+for(i in projects) {
+  pipelineJob("shiro-4-${i}") {
+  triggers {
+    gitHubPushTrigger()
+  }
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url("${gitprefix}${i}")
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
