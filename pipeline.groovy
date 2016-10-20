a743aa5d89f5904713672a563f991142127b1772
Ben Hosmer <ben.hosmer@gmail.com>
2016-10-20 14:46:25 -0400

    Add support for beachfront

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,28 +1,52 @@
-#!groovy
-// This is the initial seed job for venicegeo which creates all of ther
-// other pipeline jobs. Each repo must contain a JenkinsFile
-// that denotes the steps to take when building.
+#!groovy                                                                           
+// This is the initial seed job for venicegeo which creates all of ther            
+// other pipeline jobs. Each repo must contain a JenkinsFile                       
+// that denotes the steps to take when building.                                   
+                                                                                   
+def gitprefix = 'https://github.com/venicegeo/'                                 
 
-def gitprefix = 'https://github.com/venicegeo/pz-'
-def projects = ['access', 'gateway', 'idam', 'ingest', 'jobcommon',
-   'jobmanager', 'search-metadata-ingest', 'search-query', 'servicecontroller']
+// PZ Projects
+def pzprojects = ['pz-access', 'pz-gateway', 'pz-idam', 'pz-ingest', 'pz-jobcommon',                
+   'pz-jobmanager', 'pz-search-metadata-ingest', 'pz-search-query', 'pz-servicecontroller'] 
 
-for(i in projects) {
-  pipelineJob("venice-pz-${i}-pipeline") {
-  triggers {
-    gitHubPushTrigger()
-  }
-  definition {
-    cpsScm {
-      scm {
-        git {
-          remote {
-            url("${gitprefix}${i}")
-            }
-          }
-        }
-      }
-    }
-  }
+for(i in pzprojects) {                                                               
+  pipelineJob("venic/piazza/${i}-pipeline") {                                                                                                     
+  triggers {                                                                    
+    gitHubPushTrigger()                                                         
+  }                                                                             
+  definition {                                                                  
+    cpsScm {                                                                    
+      scm {                                                                     
+        git {                                                                   
+          remote {                                                              
+            url("${gitprefix}${i}")                                             
+            }                                                                   
+          }                                                                     
+        }                                                                       
+      }                                                                         
+    }                                                                           
+  }                                                                                
+}
+
+// BF Projects
+def bfprojects = ['bf_TidePrediction']
+
+for(i in bfprojects) {                                                               
+  pipelineJob("venice/beachfront/${i}-pipeline") {                                                                                                     
+  triggers {                                                                    
+    gitHubPushTrigger()                                                         
+  }                                                                             
+  definition {                                                                  
+    cpsScm {                                                                    
+      scm {                                                                     
+        git {                                                                   
+          remote {                                                              
+            url("${gitprefix}${i}")                                             
+            }                                                                   
+          }                                                                     
+        }                                                                       
+      }                                                                         
+    }                                                                           
+  }                                                                                
 }
 
