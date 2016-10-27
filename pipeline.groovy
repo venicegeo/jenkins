853a57ce348b4a995ab117365f0aa71b4e3cce4b
Ben Hosmer <ben.hosmer@gmail.com>
2016-10-27 13:09:56 -0400

    Adding pz-sak to pipeline projects

--- pipeline.groovy
+++ pipeline.groovy
@@ -7,7 +7,8 @@ def gitprefix = 'https://github.com/venicegeo/'
 
 // PZ Projects
 def pzprojects = ['pz-access', 'pz-gateway', 'pz-idam', 'pz-ingest', 'pz-jobcommon',                
-   'pz-jobmanager', 'pz-search-metadata-ingest', 'pz-search-query', 'pz-servicecontroller'] 
+   'pz-jobmanager', 'pz-search-metadata-ingest', 'pz-search-query', 'pz-servicecontroller',
+   'pz-sak'] 
 
 for(i in pzprojects) {
   pipelineJob("venice/piazza/${i}-pipeline") {
