35cbbb48c18be82d12c7f109b9c4a61ed8f170e0
bbarden <bbarden@users.noreply.github.com>
2016-11-30 13:28:51 -0500

    Adding pzsvc-ndwi-py

--- Repos.groovy
+++ Repos.groovy
@@ -64,6 +64,11 @@ class Repos {
       manual: ['gitlab_push'],
       pipeline: ['archive']
     ],[
+      reponame: 'pzsvc-ndwi-py',
+      team: 'beachfront',
+      manual: ['archive', 'cf_push', 'cf_bg_deploy', 'cf_promote_to_stage', 'cf_promote_to_prod'],
+      pipeline: ['archive', 'cf_push_int', 'cf_bg_deploy_int', 'beachfront_integration_tests']
+    ],[
       reponame: 'pzsvc-ossim',
       team: 'beachfront',
       manual: ['archive', 'cf_push', 'cf_bg_deploy', 'cf_promote_to_stage', 'cf_promote_to_prod'],
