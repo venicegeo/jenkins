c884d9bbfaeb05c63fdf800de4f313dd9ffcf3a6
David Bazile <dbazile@users.noreply.github.com>
2017-02-06 11:53:36 -0500

    Remove Java-centric Fortify step from bf-api

--- Repos.groovy
+++ Repos.groovy
@@ -18,7 +18,7 @@ class Repos {
       reponame: 'bf-api',
       team: 'beachfront',
       manual: ['sonar', 'archive', 'cf_push', 'cf_bg_deploy', 'cf_promote_to_stage', 'cf_promote_to_prod'],
-      pipeline: ['sonar', 'archive', 'cf_push_int', 'zap', 'cf_bg_deploy_int', 'beachfront_integration_tests', 'cf_push_stage', 'cf_bg_deploy_stage', 'fortify']
+      pipeline: ['sonar', 'archive', 'cf_push_int', 'zap', 'cf_bg_deploy_int', 'beachfront_integration_tests', 'cf_push_stage', 'cf_bg_deploy_stage']
     ],[
       reponame: 'bf-ia-broker',
       team: 'beachfront',
