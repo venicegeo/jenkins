00efb00fc38c666377464506c90cdee5c6945f1e
afroje-reshma <afroje.reshma@gmail.com>
2017-05-04 12:00:43 -0400

    Add geojson-geopkg-converter

--- pipeline.groovy
+++ pipeline.groovy
@@ -32,7 +32,7 @@ for(i in pzprojects) {
 }
 
 // BF Projects
-def bfprojects = ['bf_TidePrediction', 'bf-ui', 'bf-swagger', 'bf-api', 'pzsvc-ndwi-py']
+def bfprojects = ['bf_TidePrediction', 'bf-ui', 'bf-swagger', 'bf-api', 'pzsvc-ndwi-py', 'bf-geojson-geopkg-converter']
 
 for(i in bfprojects) {
   pipelineJob("venice/beachfront/${i}-pipeline") {
