a4bc3b7feebf330f50babd1e55a30c6dbca69986
sbaxter <seth@neuralux.com>
2017-05-10 13:15:53 -0400

    Add pz-gocommon

--- pipeline.groovy
+++ pipeline.groovy
@@ -6,7 +6,7 @@
 def gitprefix = 'https://github.com/venicegeo/'
 
 // PZ Projects
-def pzprojects = ['pz-access', 'pz-docs', 'pz-gateway', 'pz-idam', 'pz-ingest', 'pz-jobcommon',
+def pzprojects = ['pz-access', 'pz-docs', 'pz-gateway', 'pz-gocommon', 'pz-idam', 'pz-ingest', 'pz-jobcommon',
    'pz-jobmanager', 'pz-search-metadata-ingest', 'pz-search-query', 'pz-servicecontroller',
    'pz-sak', 'pz-swagger']
 
