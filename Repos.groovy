359a371c947149414ccbb339c47689641ee6c3b1
sbaxter <seth@neuralux.com>
2017-05-10 15:51:02 -0400

    Remove another

--- Repos.groovy
+++ Repos.groovy
@@ -109,11 +109,6 @@ class Repos {
       manual: ['gitlab_push'],
       pipeline: ['sonar', 'ionchannel_pom', 'archive', 'cf_push_int', 'zap', 'cf_bg_deploy_int', 'run_integration_tests', 'cf_push_stage', 'cf_bg_deploy_stage', 'run_integration_tests_stage']
     ],[
-      reponame: 'pzsvc-preview-generator',
-      team: 'piazza',
-      manual: ['gitlab_push'],
-      pipeline: ['ionchannel_pom', 'archive', 'cf_push_int', 'zap', 'cf_bg_deploy_int', 'run_integration_tests', 'cf_push_stage', 'cf_bg_deploy_stage','run_integration_tests_stage']
-    ],[
       reponame: 'pzsvc-gdaldem',
       team: 'pointcloud',
       manual: ['cf_push', 'cf_bg_deploy', 'cf_promote_to_stage', 'cf_promote_to_prod'],
