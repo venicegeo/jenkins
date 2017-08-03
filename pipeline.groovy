65c5759b016101c6ba766367ba432fb677355cf4
Ben Hosmer <ben.hosmer@gmail.com>
2017-08-03 13:40:45 -0400

    Maybe import?

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,6 +3,7 @@
 // other pipeline jobs. Each repo must contain a JenkinsFile
 // that denotes the steps to take when building.
 
+import beachfront
 evaluate(new File("./beachfront.groovy"))
 evaluate(new File("./piazza.groovy"))
 
