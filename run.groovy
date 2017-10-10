aae45f13810fe30776dc016a36071a474c31cf8f
EC2 Default User <ec2-user@ip-172-31-28-246.us-west-2.compute.internal>
2017-10-10 17:16:40 +0000

    Remove bftest-integration folder

--- run.groovy
+++ run.groovy
@@ -459,30 +459,30 @@ bf_dev_rollout.with {
 // HACKS FOR INTEGRATION TESTS
 
 // bf integration test repo
-folder("venice/beachfront/bftest-integration") {
-  displayName("bftest-integration")
-}
+//folder("venice/beachfront/bftest-integration") {
+//  displayName("bftest-integration")
+//}
 
 // bf integration test repo
-bf_gh_integration_test_job = job("venice/beachfront/bftest-integration/beachfront") //craigtest2
+//bf_gh_integration_test_job = job("venice/beachfront/bftest-integration/beachfront") //craigtest2
 
-new Base(
-  jobject: bf_gh_integration_test_job,
-  config: [
-    gh_org: 'venicegeo',
-    gh_repo: 'bftest-integration',
-    gh_branch: 'master',
-    slack_token: binding.variables.get("SLACK_TOKEN"),
-    slack_domain: "venicegeo",
-    domains: ['test.geointservices.io', 'stage.geointservices.io', 'dev.geointservices.io', 'int.geointservices.io', 'geointservices.io'],
-    domains_description: 'PCF Domain/Space to target<br>&nbsp;&nbsp;<b>geointservices.io</b>: production<br>&nbsp;&nbsp;<b>stage.geointservices.io</b>: beta<br>&nbsp;&nbsp;<b>int.geointservices.io</b>: CI<br>&nbsp;&nbsp;<b>dev.geointservices.io</b>: developer sandbox<br>&nbsp;&nbsp;<b>test.geointservices.io</b>: test bed'
-  ]
-).parameters().defaults().github().bfuapasswords()
+//new Base(
+//  jobject: bf_gh_integration_test_job,
+//  config: [
+//    gh_org: 'venicegeo',
+//    gh_repo: 'bftest-integration',
+//    gh_branch: 'master',
+//    slack_token: binding.variables.get("SLACK_TOKEN"),
+//    slack_domain: "venicegeo",
+//    domains: ['test.geointservices.io', 'stage.geointservices.io', 'dev.geointservices.io', 'int.geointservices.io', 'geointservices.io'],
+//    domains_description: 'PCF Domain/Space to target<br>&nbsp;&nbsp;<b>geointservices.io</b>: production<br>&nbsp;&nbsp;<b>stage.geointservices.io</b>: beta<br>&nbsp;&nbsp;<b>int.geointservices.io</b>: CI<br>&nbsp;&nbsp;<b>dev.geointservices.io</b>: developer sandbox<br>&nbsp;&nbsp;<b>test.geointservices.io</b>: test bed'
+//  ]
+//).parameters().defaults().github().bfuapasswords()
 
 
-def bf_gh_integration_steps = new Steps(
-  jobject: bf_gh_integration_test_job,
-  config: global_config,
-  jobname: "beachfront"
-).init().blackbox().job_script().git_checkout().gh_trigger().bf_test_secrets()
+//def bf_gh_integration_steps = new Steps(
+//  jobject: bf_gh_integration_test_job,
+//  config: global_config,
+//  jobname: "beachfront"
+//).init().blackbox().job_script().git_checkout().gh_trigger().bf_test_secrets()
 
