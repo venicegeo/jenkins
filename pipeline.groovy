e9ff95423a2f8c1d84cabe62f0ef58e40e2b2d07
Patrick D <go0ty@users.noreply.github.com>
2017-11-20 14:42:27 -0500

    Enable Phase Two Deployments

--- pipeline.groovy
+++ pipeline.groovy
@@ -109,7 +109,7 @@ for(i in pzprojects) {
       stringParam("INTEGRATION_GITLAB_CREDS", "gitlab-piazza-jenkins", "credentials for integration test repo in gitlab")
       booleanParam("SKIP_INTEGRATION_TESTS", false, "Skipping postman tests")
 	  booleanParam("SKIP_SCANS", true, "Disabling the running of static/security scans")
-      booleanParam("DEPLOY_PHASE_TWO", false, "Perform two phase CF deployment")
+      booleanParam("DEPLOY_PHASE_TWO", true, "Perform two phase CF deployment")
       booleanParam("SECENV", false, "Enable security banner and configurations")
       if (i.requiresTagging) {
         booleanParam("TAG_AND_RELEASE", false, "Tag and release all repos to bump versions")
@@ -282,7 +282,7 @@ for(i in bfprojects) {
       }
       booleanParam("SKIP_INTEGRATION_TESTS", false, "Skipping postman tests")
       booleanParam("SKIP_SCANS", true, "Disabling the running of static/security scans")
-      booleanParam("DEPLOY_PHASE_TWO", false, "Perform two phase CF deployment")
+      booleanParam("DEPLOY_PHASE_TWO", true, "Perform two phase CF deployment")
       booleanParam("SECENV", false, "Enable security banner and configurations")
       credentialsParam("CONSENT_BANNER_TEXT") {
         defaultValue("824eee31-0408-49e2-9a7e-70b59297b1b9")
