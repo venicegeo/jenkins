d7a4f893bc7fd4a3ec995497ac639a80f3046d37
Mark <mnkolev@gmail.com>
2017-11-29 11:21:15 -0500

    added promote logic

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,18 +3,21 @@
 // This is the seed job that creates all of the other jobs
 // By adding to the venice.json, you can create new jobs
 // and update parameters without needing to edit this file
-folder("venice/l2-test")
-folder("venice/l2-test/piazza")
-//remove above "test" folder when it passes
 
 
 String configfile = readFileFromWorkspace("venice.json")
 
+def folder = "venice/l2-test"
 def slurper = new groovy.json.JsonSlurper()
 def veniceprojects = slurper.parseText(configfile)
 
+folder(folder)
+folder("{$folder}/piazza")
+folder("{$folder}/beachfront")
+//remove above "test" folder when it passes
+
 for (project in veniceprojects.projects) {
-  folder("venice/l2-test/${project.foldername}") {
+  folder(folder+"${project.foldername}") {
     displayName("${project.foldername} pipelines")
   }
   for (repo in project.repos) {
@@ -24,9 +27,9 @@ for (project in veniceprojects.projects) {
         gitlabPush()
       }
       environmentVariables {
-      if (repo.threadfixId) {
-        env('THREADFIX_ID', "${repo.threadfixId}")
-      }
+        if (repo.threadfixId) {
+          env('THREADFIX_ID', "${repo.threadfixId}")
+        }
       }
       definition {
         cpsScm {
@@ -35,8 +38,41 @@ for (project in veniceprojects.projects) {
             git {
               remote {
                 url("${repo.url}")
-                branch("*/UC-Stable")
-                credentials("aaef610c-9fd0-4812-9027-755ff3a872a5")
+                branch("*/master")
+              }
+            }
+          }
+        }
+      }
+      parameters {
+        for(param in project.jobparams) {
+          if (param.type == "booleanParam") {
+          "${param.type}"("${param.name}", "${param.defaultvalue}".toBoolean(), "${param.description}")
+          } else {
+          "${param.type}"("${param.name}", "${param.defaultvalue}", "${param.description}")
+          }
+        }
+        stringParam("GIT_URL", "${repo.url}", "Git repository URL")
+        for(credparam in project.credparams) {
+          credentialsParam("${credparam.name}") {
+            defaultValue("${credparam.defaultvalue}")
+            description("${credparam.description}")
+          }
+        }
+      }
+    }
+
+    //promotion jobs
+    pipelineJob("venice/l2-test/${project.foldername}/${repo.name}-promote-pipeline") {
+      description("${repo.name} promotion pipeline")
+      definition {
+        cpsScm {
+          scm {
+            scriptPath("JenkinsFile.Promote")
+            git {
+              remote {
+                url("${repo.url}")
+                branch("*/master")
               }
             }
           }
