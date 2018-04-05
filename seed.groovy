e77f6cc7d7d9c3243362fc29d9192bd35004e129
Patrick D <patrick.doody@rgi-corp.com>
2018-04-05 11:29:24 -0400

    Refactor

--- seed.groovy
+++ seed.groovy
@@ -7,8 +7,8 @@
 String configfile = readFileFromWorkspace("seed-repos.json")
 def slurper = new groovy.json.JsonSlurper()
 def config = slurper.parseText(configfile)
-def baseFolderName = "${config.baseFolderName}"
-def promotionFolderName = "${promotionsFolderName}"
+def baseFolderName = config.baseFolderName
+def promotionFolderName = config.promotionsFolderName
 folder("${baseFolderName}")
 
 // For each folder, create the repo jobs
